b0ded9631f649ca8e6453dfb445b7f52715d6bf2
reismgadsden <rgadsden287@gmail.com>
2022-02-22 17:51:09 -0500

    Implemented memoized recursive solution

--- dynamic_pipe_cutting.py
+++ dynamic_pipe_cutting.py
@@ -1,27 +1,47 @@
 """
 Recursive problem to solve the rod cutting problem.
-Use memoization to improve runtime
+Use memoization to improve runtime.
+
+author: Reis Gadsden
+version: 22/02/2022
+
+class: CS-5531 @ Appalachian State University
+instructor: Mohammad Mohebbi
 """
 
 
 def main() -> None:
-    price_arr = [0, 1, 5, 8 , 9, 10, 17, 17, 20]
-    n = 8
+    price_arr = [1, 5, 8, 9, 10, 17, 17, 20]
+    n = 24
+
+    memo_arr = []
+    for i in range(0, n + 1):
+        memo_arr.append(-1)
+
     print(cut_rod(n, price_arr))
+    print(cut_rod_memo(n, price_arr, memo_arr))
     return
 
 
 def cut_rod(n, price_arr) -> int:
     if n == 0:
         return 0
+    val = 0
+    for i in range(0, min(n, len(price_arr))):
+        val = max(val, price_arr[i] + cut_rod(n - i - 1, price_arr))
+    return val
+
+
+def cut_rod_memo(n, price_arr, memo_arr) -> int:
+    if memo_arr[n - 1] >= 0:
+        return memo_arr[n - 1]
+    if n == 0:
+        return n
     q = 0
-    for i in range(1, n + 1):
-        q = max(q, price_arr[i] + cut_rod(n - i, price_arr))
+    for i in range(0, min(n, len(price_arr))):
+        q = max(q, price_arr[i] + cut_rod_memo(n - i - 1, price_arr, memo_arr))
+    memo_arr[n - 1] = q
     return q
-
-
-def cut_rod_memo(n, price_arr) -> int:
-    pass
 
 
 if __name__ == "__main__":

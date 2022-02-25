5ff369979949c6cfd8d868aa77556b62e027cc28
reismgadsden <rgadsden287@gmail.com>
2022-02-25 13:31:20 -0500

    Added a method that graphs a line plot so run times can be compared. Allowed methods to run over multiple iterations so that a better runtime can be determined. Cleaned up main method so that user input is a bit cleaner.

--- dynamic_pipe_cutting.py
+++ dynamic_pipe_cutting.py
@@ -10,6 +10,7 @@ instructor: Mohammad Mohebbi
 """
 
 
+from datetime import datetime
 import timeit
 import re
 import matplotlib.pyplot as plt
@@ -18,11 +19,8 @@ import numpy as np
 
 def run_time_tests(iters, samples) -> list:
     price_arr = [1, 5, 8, 9, 10, 17, 17, 20]
-    time_list = []
-    time_list.append([])
-    time_list.append([])
+    time_list = [[], []]
     for n in iters:
-        n = int(n)
         recur_run_time = 0
         memo_run_time = 0
         for i in range(0, samples):
@@ -71,7 +69,6 @@ def cut_rod_memo(n, price_arr, memo_arr) -> int:
 def compare_bars(time_list, labels, sample):
     fig, ax = plt.subplots()
     x = np.arange(len(labels))
-    print(x)
     width = 0.7 / len(labels)
 
     recur_bar = ax.bar(x - width/2, time_list[0], width, label="Recursion")
@@ -79,7 +76,7 @@ def compare_bars(time_list, labels, sample):
 
     ax.set_ylabel("Average run time over " + str(sample) + " total iterations")
     ax.set_xticks(x, labels)
-    ax.set_title("Recursion vs. Recursion w/ Memoization Timings")
+    ax.set_title("Recursion vs. Recursion w/ Memoization Run Times")
     ax.legend()
     plt.xlabel("Pipe length")
 
@@ -87,20 +84,61 @@ def compare_bars(time_list, labels, sample):
     ax.bar_label(memo_bar, padding=3)
 
     fig.tight_layout()
+    plt.yscale("log")
 
-    plt.show()
+    current_date = str(datetime.today()).strip().replace(" ", "_").replace("-", "_").replace(":", "_")
+    plt.savefig("output/bar_" + current_date[0: current_date.index(".")] + ".png")
+
+
+def compare_lines(time_list, labels, sample) -> None:
+    x = np.arange(1, len(labels) + 1)
+
+    plt.plot(x, time_list[0], label="Recursion")
+    plt.plot(x, time_list[1], label="Recursion w/ Memoization")
+    plt.legend()
+    plt.xlabel("Pipe length")
+    plt.ylabel("Average run time over " + str(sample) + " total iterations")
+    plt.title("Recursion vs. Recursion w/ Memoization Run Times")
+    plt.xticks(x)
+    plt.yscale("log")
+
+    current_date = str(datetime.today()).strip().replace(" ", "_").replace("-", "_").replace(":", "_")
+    plt.savefig("output/line_" + current_date[0: current_date.index(".")] + ".png")
 
 
 def main() -> None:
     print("Timing the \"Dynamic Pipe Cutting\" Problem!\n")
-    pattern = re.compile("^([0-9]*\\s)*[0-9]+$")
     iters = []
+    chart_type = 0
+
     while True:
-        user_in = input("Please enter the numbers (seperated by a space) you want to test for: ").strip()
-        if re.fullmatch(pattern, user_in):
-            iters = user_in.split(" ")
+        user_in = input("1. Bar\n2. Line\nPlease enter the number of the graph you wish to generate: ")
+        if user_in.strip()[0] == '1':
+            while True:
+                pattern = re.compile("^([0-9]*\\s)*[0-9]+$")
+                user_in = input("Please enter the numbers (seperated by a space) you want to test for: ").strip()
+                if re.fullmatch(pattern, user_in):
+                    chart_type = 1
+                    iters = user_in.split(" ")
+                    break
+                print("Invalid input, please try again!")
+            for i in iters[:]:
+                iters[iters.index(i)] = int(i)
+            iters.sort()
             break
-        print("Invalid input, please try again!")
+        elif user_in.strip()[0] == '2':
+            print("The line test will test values between 1 and N")
+            while True:
+                pattern = re.compile("^[1-9]([0-9]*)$")
+                user_in = input("Please enter the value of N: ").strip()
+                if re.fullmatch(pattern, user_in):
+                    chart_type = 2
+                    iters = list(range(1, int(user_in) + 1))
+                    break
+                print("Invalid input, please try again!")
+            break
+        else:
+            print("Invalid input, please try again.")
 
     sample = 0
     pattern = re.compile("^[1-9]([0-9]*)$")
@@ -112,7 +150,14 @@ def main() -> None:
         print("Invalid input please try again!")
 
     time_list = run_time_tests(iters, sample)
-    compare_bars(time_list, iters, sample)
+
+    if chart_type == 1:
+        compare_bars(time_list, iters, sample)
+    elif chart_type == 2:
+        compare_lines(time_list, iters, sample)
+    else:
+        print("wat")
+        exit(-1)
 
 
 if __name__ == "__main__":

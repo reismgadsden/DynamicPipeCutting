c2038f945f93c717266f12030efd36a0b3297ceb
reismgadsden <rgadsden287@gmail.com>
2022-02-25 16:57:50 -0500

    Added a dynamic programming solution and updated the graphing methods to include this solution

--- dynamic_pipe_cutting.py
+++ dynamic_pipe_cutting.py
@@ -1,45 +1,70 @@
 """
 Recursive problem to solve the rod cutting problem.
-Use memoization to improve runtime.
+Use memorization to improve runtime.
 
 author: Reis Gadsden
-version: 22/02/2022
+version: 25/02/2022
+github: https://github.com/reismgadsden/DynamicPipeCutting
 
 class: CS-5531 @ Appalachian State University
 instructor: Mohammad Mohebbi
 """
 
 
-from datetime import datetime
-import timeit
-import re
-import matplotlib.pyplot as plt
-import numpy as np
+# necessary imports
+from datetime import datetime # used to generate dates for file saving
+import timeit # used to time the methods (time did not work on my machine)
+import re # used to validate user input
+import matplotlib.pyplot as plt # used to generate graphs
+import numpy as np # used to generate ranges for graphs
 
 
 def run_time_tests(iters, samples) -> list:
+
+    # price array contains prices for pipes
+    #time_list is the container that holds the times for both methods
+    #time_list[0] holds the time for the recursive method
+    #time_list[1] holds the time for the recursive w/ memorization method
     price_arr = [1, 5, 8, 9, 10, 17, 17, 20]
-    time_list = [[], []]
+    time_list = [[], [], []]
+
+    # run this loop for every pipe length the user gave as input
     for n in iters:
+        # values to hold the run time of both methods over each iteration
         recur_run_time = 0
         memo_run_time = 0
+        dp_run_time = 0
+
+        # run this loop for the total number of iterations given by the user
         for i in range(0, samples):
+
+            # clear the memo array as to not effect the run time of the memorization method
+            # we want the run time in a vacuum not using values computed in the last iteration
             memo_arr = []
             for i in range(0, n + 1):
                 memo_arr.append(-1)
 
+            # time the regular recursive method and add it to the run time
             start = timeit.default_timer()
             max_recur = cut_rod(n, price_arr)
             end = timeit.default_timer()
             recur_run_time += (end - start)
 
+            # time the memorized method and add it to the run time
             start = timeit.default_timer()
             max_memo = cut_rod_memo(n, price_arr, memo_arr)
             end = timeit.default_timer()
             memo_run_time += (end - start)
 
+            start = timeit.default_timer()
+            max_dp = cut_rod_dp(n, price_arr)
+            end = timeit.default_timer()
+            dp_run_time += (end - start)
+
+        # append the averages of the run times of both methods to their respective lists
         time_list[0].append(recur_run_time / samples)
         time_list[1].append(memo_run_time / samples)
+        time_list[2].append(dp_run_time / samples)
 
 
     return time_list
@@ -66,22 +91,36 @@ def cut_rod_memo(n, price_arr, memo_arr) -> int:
     return q
 
 
+def cut_rod_dp(n, price_arr) -> int:
+    dp_arr = [0 for x in range(0, n)]
+
+    q = -1
+    for i in range(0, n):
+        for j in range(min(i + 1, len(price_arr))):
+            q = max(q, price_arr[j] + dp_arr[i - j - 1])
+        dp_arr[i] = q
+
+    return dp_arr[n - 1]
+
+
 def compare_bars(time_list, labels, sample):
     fig, ax = plt.subplots()
     x = np.arange(len(labels))
     width = 0.7 / len(labels)
 
-    recur_bar = ax.bar(x - width/2, time_list[0], width, label="Recursion")
-    memo_bar = ax.bar(x + width/2, time_list[1], width, label="Recursion w/ Memoization")
+    recur_bar = ax.bar(x - width, time_list[0], width, label="Recursion")
+    memo_bar = ax.bar(x, time_list[1], width, label="Recursion w/ Memorization")
+    dp_bar = ax.bar(x + width, time_list[2], width, label="Dynamic Programming")
 
     ax.set_ylabel("Average run time over " + str(sample) + " total iterations")
     ax.set_xticks(x, labels)
-    ax.set_title("Recursion vs. Recursion w/ Memoization Run Times")
+    ax.set_title("Recursion vs. Recursion w/ Memorization vs. Dynamic Programming Run Times")
     ax.legend()
     plt.xlabel("Pipe length")
 
     ax.bar_label(recur_bar, padding=3)
     ax.bar_label(memo_bar, padding=3)
+    ax.bar_label(dp_bar, padding=3)
 
     fig.tight_layout()
     plt.yscale("log")
@@ -90,15 +129,17 @@ def compare_bars(time_list, labels, sample):
     plt.savefig("output/bar_" + current_date[0: current_date.index(".")] + ".png")
 
 
+
 def compare_lines(time_list, labels, sample) -> None:
     x = np.arange(1, len(labels) + 1)
 
     plt.plot(x, time_list[0], label="Recursion")
-    plt.plot(x, time_list[1], label="Recursion w/ Memoization")
+    plt.plot(x, time_list[1], label="Recursion w/ Memorization")
+    plt.plot(x, time_list[2], label="Dynamic Programming")
     plt.legend()
     plt.xlabel("Pipe length")
     plt.ylabel("Average run time over " + str(sample) + " total iterations")
-    plt.title("Recursion vs. Recursion w/ Memoization Run Times")
+    plt.title("Recursion vs. Recursion w/ Memorization vs. Dynamic Programming Run Times")
     plt.xticks(x)
     plt.yscale("log")
 
